fb56dd4a04eecdfb644a5956807def48f1343bb7
n.komarov <komarovn3@gmail.com>
2024-01-27 23:48:54 +0300

    Body refactoring

--- logger/logger.c
+++ logger/logger.c
@@ -1,65 +1,78 @@
-//
-//  logger.c
-//  logger
-//
-//  Created by Николай Комаров on 21.01.2024.
-//
+#include <execinfo.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
 
 #include "logger.h"
-#include <execinfo.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 
-#include <sys/stat.h>
+#define DATE_BUF_SIZE      32
+#define MESSAGE_BUF_SIZE   256
+#define INTERR_BUF_SIZE    256
+#define BACKTRACE_BUF_SIZE 256
 
-#define DATE_BUF_SIZE 32
-#define TIME_BUF_SIZE 32
-#define MESSAGE_BUF_SIZE  512
-#define INTERR_BUF_SIZE  128
-#define BACKTRACE_BUF_SIZE 128
+const char* level_tag[LOGLEVEL_COUNT] = {
+    "DEBUG",
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "PANIC"
+};
 
 static struct logger_src {
-    logger_type type;
+    logger_output out_type;
     FILE* out_stream;
     
     char* date_buf;
-    char* time_buf;
     char* message_buf;
     void* backtrace_buf;
     
-    char* int_err;
+    char* err_message;
     void (*error_callback)(void);
 
     void (*writer)(void);
+    void (*message_former)(const char* message, logger_level level, const char* file, const char* func, const int line) ;
 } log_obj;
 
 static void log_error_callback_default(void) {}
 
+static void log_write_int_err(const char* message, ...)
+{
+    va_list va;
+    va_start(va, message);
+    vsprintf(log_obj.err_message, message, va);
+    va_end(va);
+    
+    log_obj.error_callback();
+}
+
 static logger_error log_buffers_init(void)
 {
-    log_obj.int_err = (char*)malloc(INTERR_BUF_SIZE * sizeof(char));
-    if (log_obj.int_err == NULL) {
+    log_obj.err_message = (char*)malloc(INTERR_BUF_SIZE * sizeof(char));
+    if (log_obj.err_message == NULL) {
         return LOGERR_LOGBUFFINIT;
     }
 
     log_obj.date_buf = (char*)malloc(DATE_BUF_SIZE * sizeof(char));
     if (log_obj.date_buf == NULL) {
-        return LOGERR_LOGBUFFINIT;
-    }
-    
-    log_obj.time_buf = (char*)malloc(TIME_BUF_SIZE * sizeof(char));
-    if (log_obj.time_buf == NULL) {
+        log_write_int_err("LOGGER_ERROR::Unable to initialise the internal buffer: date_buf, %ldB\n",
+                DATE_BUF_SIZE * sizeof(char));
         return LOGERR_LOGBUFFINIT;
     }
 
     log_obj.message_buf = (char*)malloc(MESSAGE_BUF_SIZE * sizeof(char));
     if (log_obj.message_buf == NULL) {
+        log_write_int_err("LOGGER_ERROR::Unable to initialise the internal buffer: message_buf, %ldB\n",
+                MESSAGE_BUF_SIZE * sizeof(char));
         return LOGERR_LOGBUFFINIT;
     }
     
-    log_obj.backtrace_buf = (char*)malloc(MESSAGE_BUF_SIZE * sizeof(char));
+    log_obj.backtrace_buf = (char*)malloc(BACKTRACE_BUF_SIZE * sizeof(char));
     if (log_obj.backtrace_buf == NULL) {
+        log_write_int_err("LOGGER_ERROR::Unable to initialise the internal buffer: backtrace_buf, %ldB\n",
+                BACKTRACE_BUF_SIZE * sizeof(char));
         return LOGERR_LOGBUFFINIT;
     }
 
@@ -69,37 +82,42 @@ static logger_error log_buffers_init(void)
 static void log_date_update(void)
 {
     if (log_obj.date_buf == NULL) {
+        log_write_int_err("LOGGER_ERROR::Date buffer uninitialised\n");
         return;
     }
     
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-
-    memset(log_obj.date_buf, 0, DATE_BUF_SIZE);
-    memset(log_obj.time_buf, 0, TIME_BUF_SIZE);
- 
-    snprintf(log_obj.date_buf, DATE_BUF_SIZE, "%i.%i.%i ", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
-    snprintf(log_obj.time_buf, TIME_BUF_SIZE, "%i:%i:%i", tm.tm_hour, tm.tm_min, tm.tm_sec);
+    size_t len = snprintf(log_obj.date_buf, DATE_BUF_SIZE, "%i.%i.%i %i:%i:%i",
+                             tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    if (len >= DATE_BUF_SIZE) {
+        log_write_int_err("LOGGER_ERROR::Date buffer overflow\n");
+        return;
+    }
 }
 
 static void log_write_std(void) {
-    fprintf(log_obj.out_stream,"%s", log_obj.message_buf);
+    size_t len = strlen(log_obj.message_buf);
+    if (fprintf(log_obj.out_stream, "%s", log_obj.message_buf) != len) {
+        log_write_int_err("LOGGER_ERROR::Unable to write to the stream\n");
+        return;
+    }
 }
 
 static void log_write_file(void) {
+    // TODO: fflush?
     size_t len = strlen(log_obj.message_buf);
     if (len != fwrite(log_obj.message_buf, len, sizeof(char), log_obj.out_stream)) {
-        // TODO
+        log_write_int_err("LOGGER_ERROR::Unable to write to log file\n");
+        return;
     }
-//    if (!fflush(log_obj.out_stream)){
-//        // TODO
-//    }
 }
 
 static logger_error log_set_out_file(const char* file_name)
 {
     FILE* f = fopen(file_name, "w");
     if (f == NULL) {
+        log_write_int_err("LOGGER_ERROR::Unable to create/open log file\n");
         return LOGERR_LOGFILECREATE;
     }
     
@@ -108,52 +126,20 @@ static logger_error log_set_out_file(const char* file_name)
     return LOGERR_NOERR;
 }
 
-static void log_form_string(const char* type, const char* file, const char* func, const int line, const char* mes)
-{
-    memset(log_obj.message_buf, 0, MESSAGE_BUF_SIZE);
-    switch (log_obj.type) {
-        case LOGTYPE_DEBUG:
-            snprintf(log_obj.message_buf,  MESSAGE_BUF_SIZE, "%s%s%s%s::%s::%i::%s\n",
-                     log_obj.date_buf, log_obj.time_buf, type, file, func, line, mes);
-            break;
-        case LOGTYPE_PRODUCT:
-            snprintf(log_obj.message_buf,  MESSAGE_BUF_SIZE, "%s%s%s%s\n",
-                     log_obj.date_buf, log_obj.time_buf, type, mes);
-            break;
-        default:
-            // TODO
-            break;
-    }
-}
-
-static int log_write_callstack(void* buffer, int size)
+static void log_write_callstack(void* buffer, int size)
 {
     char **strings = backtrace_symbols(buffer, size);
     if (strings == NULL) {
-        perror("backtrace_symbols");
-        exit(EXIT_FAILURE);
+        log_write_int_err("LOGGER_ERROR::Unable to initialise the internal buffer: backtrace_buf, %dB\n", size);
+        return;
     }
 
-    memset(log_obj.message_buf, 0, MESSAGE_BUF_SIZE);
     for (int i = 0, j = 0; i < size; i++) {
         sprintf(&log_obj.message_buf[j], "%s\n", strings[i]);
         j += strlen(strings[i]) + 1;
     }
-    
-//    for (int i = 0; i < size; ++i){
-//        if (strlen(strings[i]) != fwrite(strings[i], 1, strlen(strings[i]), f)){
-//            printf("logging_error: Error writing Error to log\n");
-//            free(strings);
-//            return 1;
-//        }
-//        fprintf(f, "\n");
-//    }
-//
-//    if (fflush(f))
-//        printf("logging_error: Error writing Error to log\n");
 
     free(strings);
-    return 0;
 }
 
 void* __log_refresh_backtrace_buf(void)
@@ -162,40 +148,76 @@ void* __log_refresh_backtrace_buf(void)
     return log_obj.backtrace_buf;
 }
 
+static void log_form_debug_message(const char* message, logger_level level, const char* file, const char* func, const int line)
+{
+    log_date_update();
+    size_t len = snprintf(log_obj.message_buf, MESSAGE_BUF_SIZE, "%s::%s::%s::%s::%i::%s\n",
+                             log_obj.date_buf, level_tag[level], file, func, line, message);
+    if (len >= MESSAGE_BUF_SIZE) {
+        log_write_int_err("LOGGER_ERROR::Message buffer overflow\n");
+        return;
+    }
+}
+
+static void log_form_product_message(const char* message, logger_level level, const char* file, const char* func, const int line)
+{
+    log_date_update();
+    size_t len = snprintf(log_obj.message_buf, MESSAGE_BUF_SIZE, "%s::%s::%s\n",
+             log_obj.date_buf, level_tag[level], message);
+    if (len >= MESSAGE_BUF_SIZE) {
+        log_write_int_err("LOGGER_ERROR::Message buffer overflow\n");
+        return;
+    }
+}
+
 logger_error log_init_default(void)
 {
-    log_obj.type = LOGTYPE_DEBUG;
+    log_obj.out_type = LOGOUT_STREAM;
     log_obj.out_stream = stderr;
     log_obj.writer = log_write_std;
     log_obj.error_callback = log_error_callback_default;
+    log_obj.message_former = log_form_debug_message;
     return log_buffers_init();
 }
 
 logger_error log_init(logger_settings* settings)
 {
     if (settings == NULL) {
+        log_write_int_err("LOGGER_WARNING::Empty settings, setting default\n");
         return log_init_default();
     }
     logger_error err = LOGERR_NOERR;
-    
-    log_obj.type = (settings->type == LOGTYPE_UNKNOWN) ? LOGTYPE_UNKNOWN : settings->type;
     log_obj.error_callback = (settings->error_callback == NULL) ? log_error_callback_default : settings->error_callback;
+    
+    switch (settings->type) {
+        case LOGTYPE_DEBUG:
+            log_obj.message_former = log_form_debug_message;
+            break;
+        case LOGTYPE_PRODUCT:
+            log_obj.message_former = log_form_product_message;
+            break;
+        default:
+            log_write_int_err("LOGGER_ERROR::Unknown logger type\n");
+            return LOGERR_LOGUNKNOWNTYPE;
+    }
     
     switch (settings->out_type) {
         case LOGOUT_STREAM:
             log_obj.out_stream = (settings->output.out_stream != stderr && settings->output.out_stream != stdout) ?
                 stderr : settings->output.out_stream;
             log_obj.writer = log_write_std;
+            
             break;
         case LOGOUT_FILE:
             err = log_set_out_file(settings->output.file_name);
             if (err != LOGERR_NOERR) {
                 return err;
             }
+            log_obj.out_type = LOGOUT_FILE;
             break;
         default:
-        // TODO
-            break;
+            log_write_int_err("LOGGER_ERROR::Unknown logger output type\n");
+            return LOGERR_LOGUNKNOWNOUTTYPE;
     }
     
     return log_buffers_init();
@@ -203,60 +225,41 @@ logger_error log_init(logger_settings* settings)
 
 void log_destruct(void)
 {
+    if (log_obj.out_type == LOGOUT_FILE) {
+        if (fclose(log_obj.out_stream) != 0) {
+            log_write_int_err("LOGGER_ERROR::Unable to close log file\n");
+        }
+    }
     free(log_obj.date_buf);
-    free(log_obj.time_buf);
-    free(log_obj.int_err);
+    free(log_obj.err_message);
     free(log_obj.message_buf);
+    free(log_obj.backtrace_buf);
 }
 
-void __log_debug(const char* file, const char* func, const int line, const char* mes)
+char* log_get_internal_error(void)
+{
+    return log_obj.message_buf;
+}
+
+void __log_log(const char* message, logger_level level, const char* file, const char* func, const int line)
 {
     if (log_obj.message_buf == NULL) {
+        log_write_int_err("LOGGER_ERROR::Message buffer uninitialised\n");
         return;
     }
 
-    log_date_update();
-    log_form_string("::DEBUG::", file, func, line, mes);
+    log_obj.message_former(message, level, file, func, line);
     log_obj.writer();
 }
 
-void __log_info(const char* file, const char* func, const int line, const char* mes)
+void __log_backtrace(bool is_panic, void* buffer, int size)
 {
     if (log_obj.message_buf == NULL) {
+        log_write_int_err("LOGGER_ERROR::Message buffer uninitialised\n");
         return;
     }
-
-    log_date_update();
-    log_form_string("::INFO::", file, func, line, mes);
-    log_obj.writer();
-}
-
-void __log_warning(const char* file, const char* func, const int line, const char* mes)
-{
-    if (log_obj.message_buf == NULL) {
-        return;
-    }
-
-    log_date_update();
-    log_form_string("::WARNING::", file, func, line, mes);
-    log_obj.writer();
-}
-
-void __log_error(const char* file, const char* func, const int line, const char* mes, void* buffer, int size)
-{
-    if (log_obj.message_buf == NULL) {
-        return;
-    }
-
-    log_date_update();
-    log_form_string("::ERROR::", file, func, line, mes);
-    log_obj.writer();
     
     log_write_callstack(buffer, size);
     log_obj.writer();
-}
-
-void __log_panic(const char* file, const char* func, const int line, const char* mes)
-{
-    
+    if (is_panic) exit(EXIT_FAILURE);
 }
--- logger/logger.h
+++ logger/logger.h
@@ -1,33 +1,46 @@
-//
-//  logger.h
-//  logger
-//
-//  Created by Николай Комаров on 21.01.2024.
-//
-
 #ifndef logger_h
 #define logger_h
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <execinfo.h>
 
 #define LOG_BACKTRACE_BUF_SIZE 128
 
-#define log_debug(message)     __log_debug(__FILE__, __func__, __LINE__, message);
-#define log_info(message)       __log_info(__FILE__, __func__, __LINE__, message);
-#define log_warning(message) __log_warning(__FILE__, __func__, __LINE__, message);
+#define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define log_log(message) __log_log(message, LOGLEVEL_INFO, __FILENAME__, __func__, __LINE__)
+#define log_debug(message)   __log_log(message, LOGLEVEL_DEBUG, __FILENAME__, __func__, __LINE__)
+#define log_info(message)    __log_log(message, LOGLEVEL_INFO, __FILENAME__, __func__, __LINE__)
+#define log_warning(message) __log_log(message, LOGLEVEL_WARNING, __FILENAME__, __func__, __LINE__)
 #define log_error(message) {\
+    __log_log(message, LOGLEVEL_ERROR, __FILENAME__, __func__, __LINE__); \
     void* buffer = __log_refresh_backtrace_buf();\
     int size = backtrace(buffer, LOG_BACKTRACE_BUF_SIZE);\
-    __log_error(__FILE__, __func__, __LINE__, message, buffer, size);\
+    __log_backtrace(false, buffer, size);\
 }
-#define log_panic(message)     __log_panic(__FILE__, __func__, __LINE__, message);
+#define log_panic(message) {\
+    __log_log(message, LOGLEVEL_PANIC, __FILENAME__, __func__, __LINE__); \
+    void* buffer = __log_refresh_backtrace_buf();\
+    int size = backtrace(buffer, LOG_BACKTRACE_BUF_SIZE);\
+    __log_backtrace(true, buffer, size);\
+}
 
 typedef enum {
     LOGTYPE_UNKNOWN = 0,
     LOGTYPE_DEBUG,
     LOGTYPE_PRODUCT,
 } logger_type;
+
+typedef enum {
+    LOGLEVEL_DEBUG = 0,
+    LOGLEVEL_INFO,
+    LOGLEVEL_WARNING,
+    LOGLEVEL_ERROR,
+    LOGLEVEL_PANIC,
+    
+    LOGLEVEL_COUNT,
+} logger_level;
 
 typedef enum {
     LOGOUT_UNKNOWN = 0,
@@ -39,6 +52,8 @@ typedef enum {
     LOGERR_NOERR = 0,
     LOGERR_LOGFILECREATE,
     LOGERR_LOGBUFFINIT,
+    LOGERR_LOGUNKNOWNTYPE,
+    LOGERR_LOGUNKNOWNOUTTYPE,
 } logger_error;
 
 typedef struct {
@@ -56,12 +71,10 @@ typedef struct logger_src* logger;
 logger_error log_init_default(void);
 logger_error log_init(logger_settings* settings);
 void log_destruct(void);
-void* __log_refresh_backtrace_buf(void);
+char* log_get_internal_error(void);
 
-void   __log_debug(const char* file, const char* func, const int line, const char* mes);
-void    __log_info(const char* file, const char* func, const int line, const char* mes);
-void __log_warning(const char* file, const char* func, const int line, const char* mes);
-void   __log_error(const char* file, const char* func, const int line, const char* mes, void* buffer, int size);
-void   __log_panic(const char* file, const char* func, const int line, const char* mes);
+void* __log_refresh_backtrace_buf(void);
+void __log_log(const char* mes, logger_level level, const char* file, const char* func, const int line);
+void __log_backtrace(bool is_panic, void* buffer, int size);
 
 #endif /* logger_h */
